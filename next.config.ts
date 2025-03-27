8505baab28a42b1896b7eac4a08abd85fd14e784
Navatusein <boghdan.kutsulima@gmail.com>
2025-03-27 15:51:24 +0200

    Added support Docker

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "standalone",
   devIndicators: {
-    buildActivityPosition: "bottom-right",
-  }
+    position: "bottom-right"
+  },
 };
 
 export default nextConfig;
