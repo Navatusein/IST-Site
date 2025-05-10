70d6cb20233066ab3fcbc3611d8a7e0b8c5b902f
Navatusein <boghdan.kutsulima@gmail.com>
2025-05-10 14:25:16 +0300

    Update env format

--- src/shared/types/env.d.ts
+++ src/shared/types/env.d.ts
@@ -1,8 +1,8 @@
 declare namespace NodeJS {
   interface ProcessEnv {
     MONGODB_URI: string;
-
-    NEXTAUTH_SECRET: string;
-    NEXTAUTH_URL: string;
+    NEXT_PUBLIC_BASE_URL: string;
+    SECRET: string;
+    ALLOWED_DEV_ORIGINS?: string;
   }
 }
