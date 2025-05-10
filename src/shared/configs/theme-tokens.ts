1e0b54c3886a02a8a0cb27d65f891d9afa6598e7
Navatusein <boghdan.kutsulima@gmail.com>
2025-05-10 18:27:27 +0300

    Add theme tokens and scss

---
+++ src/shared/configs/theme-tokens.ts
@@ -0,0 +1,9 @@
+export const themeTokens = {
+  "colorPrimary": "#106a83",
+  "colorInfo": "#106a83",
+  "borderRadius": 4,
+  "wireframe": false,
+  "fontSize": 16,
+  "colorSuccess": "#2f623e",
+  "colorError": "#f5222d",
+}
