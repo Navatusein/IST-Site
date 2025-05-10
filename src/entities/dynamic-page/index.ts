1d26d8c6789236f0ec38c901c2dcfaedd406370d
Navatusein <boghdan.kutsulima@gmail.com>
2025-05-10 18:25:06 +0300

    Add types for page components examples

--- src/entities/dynamic-page/index.ts
+++ src/entities/dynamic-page/index.ts
@@ -1,6 +1,6 @@
 import DynamicPageModel, {dynamicPageSchema} from "./types/schema";
-import {IBasePageComponent, IDynamicPage, PageComponentType} from "./types/type";
+import {IBasePageComponent, IDynamicPage, IPageComponentExample, PageComponentType} from "./types/type";
 
 
-export type {IDynamicPage, IBasePageComponent, PageComponentType};
+export type {IDynamicPage, IBasePageComponent, PageComponentType, IPageComponentExample};
 export {DynamicPageModel, dynamicPageSchema};
--- src/entities/dynamic-page/types/type.ts
+++ src/entities/dynamic-page/types/type.ts
@@ -1,6 +1,6 @@
 import {Document} from "mongoose";
 
-export type PageComponentType = "text"
+export type PageComponentType = "text"|"hero-section"|"news-list"|"title";
 
 export interface IBasePageComponent {
   id: string;
@@ -12,4 +12,9 @@ export interface IDynamicPage extends Document {
   title: string;
   path: string;
   components: IBasePageComponent[];
+}
+
+export interface IPageComponentExample {
+  name: string;
+  component: IBasePageComponent;
 }
