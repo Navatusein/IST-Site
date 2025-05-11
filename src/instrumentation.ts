ffe97796134c4af8711b23402baf3c2e0bee6982
Navatusein <boghdan.kutsulima@gmail.com>
2025-05-11 13:27:00 +0300

    Fix url in instrumentation

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,6 +1,8 @@
+import normalizeUrl from "normalize-url";
+
 export function register() {
   if (process.env.NEXT_RUNTIME === "nodejs") {
-    fetch("http://localhost:3000/api/init")
+    fetch(normalizeUrl(`${process.env.NEXT_PUBLIC_BASE_URL}/api/init`))
       .then(async response => {
         console.log(await response.json());
       })
