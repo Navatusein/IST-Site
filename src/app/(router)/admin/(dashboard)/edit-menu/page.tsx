9781a0a552dad58d0cedab07951c8fe9b334de74
Navatusein <boghdan.kutsulima@gmail.com>
2025-05-17 23:13:21 +0300

    Removed link linkedin

--- src/app/(router)/admin/(dashboard)/edit-menu/page.tsx
+++ src/app/(router)/admin/(dashboard)/edit-menu/page.tsx
@@ -1,8 +1,9 @@
 import {getPublicMenuItemsTreeAction} from "@/entities/public-menu-item/actions/actions";
 import {AdminPublicMenuControlView} from "@/views/admin-public-menu-control";
+import {useServerAction} from "@/shared/hooks/use-server-action";
 
 export default async function Page() {
-  const menuItems = await getPublicMenuItemsTreeAction();
+  const menuItems = await useServerAction(getPublicMenuItemsTreeAction());
 
   return (
     <AdminPublicMenuControlView menuItems={menuItems}/>
