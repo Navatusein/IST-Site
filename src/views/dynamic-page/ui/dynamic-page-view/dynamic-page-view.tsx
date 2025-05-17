e65702775189bf27b3783c0b3c5f727827f25639
Navatusein <boghdan.kutsulima@gmail.com>
2025-05-17 22:08:51 +0300

    Fixed dynamic-page-renderer name

--- src/views/dynamic-page/ui/dynamic-page-view/dynamic-page-view.tsx
+++ src/views/dynamic-page/ui/dynamic-page-view/dynamic-page-view.tsx
@@ -8,7 +8,7 @@ interface IProps {
   page: IDynamicPage
 }
 
-const DynamicPageRenderer = dynamic(() => import("@/views/dynamic-page/ui/dynamic-page-dynamic-view/dynamic-page-renderer"), {
+const DynamicPageRenderer = dynamic(() => import("@/views/dynamic-page/ui/dynamic-page-renderer/dynamic-page-renderer"), {
   ssr: false,
   loading: () => <Loader/>
 });
