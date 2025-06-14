13549ede5f381ff1aba1c0c192ce9a08b6f03e3e
Navatusein <boghdan.kutsulima@gmail.com>
2025-06-14 17:48:11 +0300

    Fixed carousel height

--- src/app/(router)/(public-pages)/test/component/component.tsx
+++ src/app/(router)/(public-pages)/test/component/component.tsx
@@ -2,7 +2,6 @@
 
 import {Card, Carousel, Image, theme} from "antd";
 import {useMemo} from "react";
-import {useMediaQuery} from "react-responsive";
 import styles from "./component.module.scss";
 
 interface IProps {
@@ -13,13 +12,6 @@ export default function Component(props: IProps) {
   const typedComponentProps = {
     type: "contact-us",
     width: props.width,
-    imagePaths: [
-      "/news-images/news-1.webp",
-      "/news-images/news-2.webp",
-      "/news-images/news-3.webp",
-      "/news-images/news-4.webp",
-      "/news-images/news-5.webp",
-    ]
   }
 
   const {token: {padding, paddingXS}} = theme.useToken();
@@ -34,41 +26,9 @@ export default function Component(props: IProps) {
     }
   }, [typedComponentProps.width])
 
-  const fullImagePaths = useMemo(() => (
-    typedComponentProps.imagePaths.map(imagePath => `/api/assets${imagePath}`
-  )), [typedComponentProps.imagePaths]);
-
-  const isXl = useMediaQuery({minWidth: 1200})
-  const isLg = useMediaQuery({minWidth: 992})
-
   return (
     <>
-      <Carousel
-        responsive={[
-          {breakpoint: 1200, settings: {slidesToShow: 3}},
-          {breakpoint: 992, settings: {slidesToShow: 2}},
-          // {breakpoint: 1200, settings: {slidesToShow: 3}}
-        ]}
-        autoplay
-        arrows
-        centerPadding={"12px"}
-      >
-        {fullImagePaths.map((imagePath, index) => (
-          <Card>
-            <Image.PreviewGroup
-              items={fullImagePaths}
-            >
-              <Image
-                key={`image-${imagePath}`}
-                className={styles.image}
-                width={"100%"}
-                src={imagePath}
-                fallback="/missing-image.webp"
-              />
-            </Image.PreviewGroup>
-          </Card>
-        ))}
-      </Carousel>
+
     </>
   )
 }
--- src/page-components/gallery-carousel/ui/gallery-carousel/gallery-carousel.tsx
+++ src/page-components/gallery-carousel/ui/gallery-carousel/gallery-carousel.tsx
@@ -27,7 +27,7 @@ export default function GalleryCarousel(props: IProps) {
       <Card variant="borderless" style={{borderRadius: 0}}>
         <Carousel
           slidesToShow={3}
-          autoplay={true}
+          autoplay={false}
           infinite={true}
           arrows={true}
           className={styles.carousel}
